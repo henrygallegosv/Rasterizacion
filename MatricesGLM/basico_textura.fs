54c786b4aa908a7f2079ff58be857bbbdd0aee44
Henry Gallegos <henrygallegosv@gmail.com>
2021-11-15 07:57:58 -0500

    multiples luces

--- MatricesGLM/basico_textura.fs
+++ MatricesGLM/basico_textura.fs
@@ -17,11 +17,6 @@ void main() {
    vec4 color = vec4(1.);
    float kd = 0.8;
 
-   //float diff = max(0.0, dot(normalize(posicionLuz - posicion.xyz/posicion.w), normal.xyz/normal.w));
-   //float diff = max(0.0, dot(normalize(posicionLuz - posicion.xyz), normal.xyz));
-   //FragColor = vec4(color.rgb * colorLuz * kd * diff, 1.0) * texture( textura0, v2f_texcoord );
-
-
     // Compute the diffuse term.
     vec4 N = normalize( normal );
     vec4 L = normalize( posicionLuz - posicion );
@@ -29,6 +24,6 @@ void main() {
     vec4 Diffuse =  NdotL * colorLuz * color;
     FragColor = Diffuse * texture( textura0, v2f_texcoord );
 
-   //FragColor = vec4(colorFinal, 1.0f);
-   //FragColor = colorFinal;
+    // Especular
+    // camara
 }
